97875a8f47dfd13e3858261b49884ad18f885ced
earlwlkr <mike358015@gmail.com>
2023-07-17 07:14:34 +0700

    Layout

--- components/ExpenseInput.tsx
+++ components/ExpenseInput.tsx
@@ -4,31 +4,41 @@ import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import { useRef } from 'react';
 import { useExpensesStore } from '@/lib/expenses';
+import { useToast } from '@/components/ui/use-toast';
 
 export function ExpenseInput() {
+  const { toast } = useToast();
   const addExpense = useExpensesStore((state) => state.add);
   const inputRef = useRef<HTMLInputElement>(null);
+
   return (
-    <>
-      <div className="py-4">
+    <div className="py-4 flex flex-col lg:flex-row">
+      <div className="lg:pr-4 pb-4 lg:pb-0">
         <Input ref={inputRef} type="text" placeholder="enter your expense..." />
       </div>
 
       <Button
         onClick={async () => {
-          const response = await fetch('/api', {
-            method: 'POST',
-            body: JSON.stringify({
-              q: inputRef.current?.value,
-            }),
-          });
-          const result = await response.json();
-          console.log('result', result);
-          addExpense(result);
+          try {
+            const response = await fetch('/api', {
+              method: 'POST',
+              body: JSON.stringify({
+                q: inputRef.current?.value,
+              }),
+            });
+            const result = await response.json();
+            console.log('result', result);
+            addExpense(result);
+          } catch (error) {
+            toast({
+              title: 'Error',
+              description: 'Cannot generate',
+            });
+          }
         }}
       >
         Add
       </Button>
-    </>
+    </div>
   );
 }
--- components/Expenses.tsx
+++ components/Expenses.tsx
@@ -94,35 +94,35 @@ export const columns: ColumnDef<Expense>[] = [
       <div className="capitalize">{row.getValue('category')}</div>
     ),
   },
-  {
-    id: 'actions',
-    enableHiding: false,
-    cell: ({ row }) => {
-      const payment = row.original;
+  // {
+  //   id: 'actions',
+  //   enableHiding: false,
+  //   cell: ({ row }) => {
+  //     const payment = row.original;
 
-      return (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="h-8 w-8 p-0">
-              <span className="sr-only">Open menu</span>
-              <MoreHorizontal className="h-4 w-4" />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuLabel>Actions</DropdownMenuLabel>
-            <DropdownMenuItem
-              onClick={() => navigator.clipboard.writeText(payment.id)}
-            >
-              Copy payment ID
-            </DropdownMenuItem>
-            {/* <DropdownMenuSeparator />
-            <DropdownMenuItem>View customer</DropdownMenuItem>
-            <DropdownMenuItem>View payment details</DropdownMenuItem> */}
-          </DropdownMenuContent>
-        </DropdownMenu>
-      );
-    },
-  },
+  //     return (
+  //       <DropdownMenu>
+  //         <DropdownMenuTrigger asChild>
+  //           <Button variant="ghost" className="h-8 w-8 p-0">
+  //             <span className="sr-only">Open menu</span>
+  //             <MoreHorizontal className="h-4 w-4" />
+  //           </Button>
+  //         </DropdownMenuTrigger>
+  //         <DropdownMenuContent align="end">
+  //           <DropdownMenuLabel>Actions</DropdownMenuLabel>
+  //           <DropdownMenuItem
+  //             onClick={() => navigator.clipboard.writeText(payment.id)}
+  //           >
+  //             Copy payment ID
+  //           </DropdownMenuItem>
+  //           <DropdownMenuSeparator />
+  //           <DropdownMenuItem>View customer</DropdownMenuItem>
+  //           <DropdownMenuItem>View payment details</DropdownMenuItem>
+  //         </DropdownMenuContent>
+  //       </DropdownMenu>
+  //     );
+  //   },
+  // },
 ];
 
 export function Expenses() {
@@ -155,13 +155,13 @@ export function Expenses() {
   });
 
   return (
-    <div className="w-full">
-      <div className="flex items-center py-4">
+    <div className="w-full py-4">
+      {/* <div className="flex items-center py-4">
         <Input
           placeholder="Filter..."
-          value={(table.getColumn('item')?.getFilterValue() as string) ?? ''}
+          value={(table.getColumn('name')?.getFilterValue() as string) ?? ''}
           onChange={(event) =>
-            table.getColumn('item')?.setFilterValue(event.target.value)
+            table.getColumn('name')?.setFilterValue(event.target.value)
           }
           className="max-w-sm"
         />
@@ -191,7 +191,7 @@ export function Expenses() {
               })}
           </DropdownMenuContent>
         </DropdownMenu>
-      </div>
+      </div> */}
       <div className="rounded-md border">
         <Table>
           <TableHeader>
@@ -242,7 +242,7 @@ export function Expenses() {
           </TableBody>
         </Table>
       </div>
-      <div className="flex items-center justify-end space-x-2 py-4">
+      {/* <div className="flex items-center justify-end space-x-2 py-4">
         <div className="flex-1 text-sm text-muted-foreground">
           {table.getFilteredSelectedRowModel().rows.length} of{' '}
           {table.getFilteredRowModel().rows.length} row(s) selected.
@@ -265,7 +265,7 @@ export function Expenses() {
             Next
           </Button>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
