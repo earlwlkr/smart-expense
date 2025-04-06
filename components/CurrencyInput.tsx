0965706d55700bb5e13606de2348ef7925aadd21
earlwlkr <mike358015@gmail.com>
2025-04-06 09:28:18 +0700

    Refactor CurrencyInput to extract currency formatting logic into a separate function

--- components/CurrencyInput.tsx
+++ components/CurrencyInput.tsx
@@ -4,32 +4,36 @@ import { Input } from './ui/input';
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
 
+const formatCurrencyValue = (value: string) => {
+  const [formattedWholeValue, decimalValue = '0'] = value.split('.');
+  const significantValue = formattedWholeValue.replace(/,/g, '');
+  const floatValue = parseFloat(
+    significantValue + '.' + decimalValue.slice(0, 2)
+  );
+  if (isNaN(floatValue) === false) {
+    // you can dispatch to your redux store here - floatValue should be
+    // a number, that's well formed
+
+    let n = new Intl.NumberFormat('en-EN', {
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 2,
+    }).format(floatValue);
+
+    if (value.includes('.') && !n.includes('.')) {
+      return n + '.';
+    }
+    return n;
+  }
+  return '0';
+};
+
 export const CurrencyInput = React.forwardRef<HTMLInputElement, InputProps>(
   ({ ...props }, ref) => {
     const [value, dispatchValue] = React.useReducer(
       (state: string, newValue: string) => {
-        const [formattedWholeValue, decimalValue = '0'] = newValue.split('.');
-        const significantValue = formattedWholeValue.replace(/,/g, '');
-        const floatValue = parseFloat(
-          significantValue + '.' + decimalValue.slice(0, 2)
-        );
-        if (isNaN(floatValue) === false) {
-          // you can dispatch to your redux store here - floatValue should be
-          // a number, that's well formed
-
-          let n = new Intl.NumberFormat('en-EN', {
-            minimumFractionDigits: 0,
-            maximumFractionDigits: 2,
-          }).format(floatValue);
-
-          if (newValue.includes('.') && !n.includes('.')) {
-            return n + '.';
-          }
-          return n;
-        }
-        return '0';
+        return formatCurrencyValue(newValue);
       },
-      ''
+      formatCurrencyValue(String(props.value))
     );
 
     return (
