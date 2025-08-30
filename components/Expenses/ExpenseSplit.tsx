c3a76e8276aeb0ca2fd61f75cd3499f8625d1bc4
earlwlkr <mike358015@gmail.com>
2025-08-30 14:29:29 +0700

    refactor: remove split tab dropdown filter

--- components/Expenses/ExpenseSplit.tsx
+++ components/Expenses/ExpenseSplit.tsx
@@ -4,21 +4,12 @@ import { Expense } from '@/lib/types';
 import {
   Table,
   TableBody,
-  TableCaption,
   TableCell,
-  TableFooter,
   TableHead,
   TableHeader,
   TableRow,
 } from '@/components/ui/table';
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from '@/components/ui/select';
-import { Fragment, useMemo, useState } from 'react';
+import { Fragment, useMemo } from 'react';
 
 function calculateSplitDetails(expenses: Expense[]) {
   const splitDetails = expenses.reduce((acc, expense) => {
@@ -63,7 +54,6 @@ export function ExpenseSplit() {
   const { items: expenses } = useExpensesStore();
   const { members } = useMembers();
   const splitDetails = calculateSplitDetails(expenses);
-  const [filter, setFilter] = useState('all');
 
   const groups = useMemo(() => {
     return Object.entries(splitDetails).map(([fromId, details]) => {
@@ -84,28 +74,8 @@ export function ExpenseSplit() {
     });
   }, [splitDetails, members]);
 
-  const filteredGroups = useMemo(() => {
-    if (filter === 'all') return groups;
-    return groups.filter((group) => group.id === filter);
-  }, [groups, filter]);
-
   return (
     <div>
-      <div className="mb-4 flex justify-end">
-        <Select onValueChange={setFilter} value={filter}>
-          <SelectTrigger className="w-64">
-            <SelectValue placeholder="Filter by participant" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem value="all">All participants</SelectItem>
-            {groups.map((group) => (
-              <SelectItem key={group.id} value={group.id}>
-                {group.from}
-              </SelectItem>
-            ))}
-          </SelectContent>
-        </Select>
-      </div>
       <Table>
         <TableHeader>
           <TableRow>
@@ -115,7 +85,7 @@ export function ExpenseSplit() {
           </TableRow>
         </TableHeader>
         <TableBody>
-          {filteredGroups.map((group) => (
+          {groups.map((group) => (
             <Fragment key={group.id}>
               {group.items.map((item, index) => (
                 <TableRow key={item.key}>
